8c8bcda8e521d07751e0b4b55fc55e92afe82acc
Melissa <Melissa-Gh03@users.noreply.github.com>
2026-06-01 15:13:18 +0300

    Electro template integrated

--- resources/views/layout/home.blade.php
+++ resources/views/layout/home.blade.php
@@ -1,56 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>@yield('title')</title>
-    <title>@yield('keywords')</title>
-    <title>@yield('description')</title>
-    @section('head')
-        @section('head')
-            <!-- Google font -->
-            <link href="https://fonts.googleapis.com/css?family=Hind:400,700" rel="stylesheet">
+    <title>@yield('title', 'Electro - Laravel Ecommerce')</title>
 
-            <!-- Bootstrap -->
-            <link type="text/css" rel="stylesheet" href="css/bootstrap.min.css" />
-
-            <!-- Slick -->
-            <link type="text/css" rel="stylesheet" href="css/slick.css" />
-            <link type="text/css" rel="stylesheet" href="css/slick-theme.css" />
-
-            <!-- nouislider -->
-            <link type="text/css" rel="stylesheet" href="css/nouislider.min.css" />
-
-            <!-- Font Awesome Icon -->
-            <link rel="stylesheet" href="css/font-awesome.min.css">
-
-            <!-- Custom stylesheet -->
-            <link type="text/css" rel="stylesheet" href="css/style.css" />
-
-            <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-            <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-            <!--[if lt IE 9]>
-            <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-            <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-            <![endif]-->
-
-        @show
-    @show
-
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}"/>
+    <link type="text/css" rel="stylesheet" href="{{ asset('css/slick.css') }}"/>
+    <link type="text/css" rel="stylesheet" href="{{ asset('css/slick-theme.css') }}"/>
+    <link type="text/css" rel="stylesheet" href="{{ asset('css/nouislider.min.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
+    <link type="text/css" rel="stylesheet" href="{{ asset('css/style.css') }}"/>
 </head>
+
 <body>
-@section('header')
-    This is the master header.
-@show
-@yield('slider')
-<div class="container">
-    @yield('content')
-</div>
-@section('footer')
-    This is the master footer.
-@show
+
+@yield('content')
+
+<script src="{{ asset('js/jquery.min.js') }}"></script>
+<script src="{{ asset('js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('js/slick.min.js') }}"></script>
+<script src="{{ asset('js/nouislider.min.js') }}"></script>
+<script src="{{ asset('js/jquery.zoom.min.js') }}"></script>
+<script src="{{ asset('js/main.js') }}"></script>
+
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -14,7 +13,7 @@ Route::get('/dashboard', function () {
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::delete('/profile', [ProfileController::class, 'destroy']);
 });
 
 require __DIR__.'/auth.php';
